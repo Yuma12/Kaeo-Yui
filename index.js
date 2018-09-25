4ed82dcba67b003060bd3ea692c107fb1500252e
Yuma12 <42080661+Yuma12@users.noreply.github.com>
2018-09-25 18:52:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = process.env.token // a garder en version heroku
+const prefix = ("?");
+
+bot.on('ready', function () {
+    console.log("Je suis prêt à être utilisé.")
+    bot.user.setActivity('rien').catch(console.error)
+});
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
+        console.log(`${member.displayName} à rejoind le serveur.`)
+    }).catch(console.error)
+});
+
+
+
+bot.on('message', msg => {
+    if (msg.content === "bonjour"){
+        msg.reply("Heureux de te revoir parmis nous.")
+    }
+    if (msg.content.match(/salut/i)) {
+            msg.reply('Je suis d\'accord avec toi.')
+    }
+    if (msg.content === prefix + "site"){
+        msg.channel.send("https://alexpgm.000webhostapp.com/")
+        console.log("Une personne a demandé pour aller sur ton site.")
+    }
+
+});
+
+bot.login(token); //a garder en version heroku
